3a0f078cea4a7f896a9d815b317b7c67cd7fec56
Roshan <rrai4959@gmail.com>
2022-10-29 00:05:34 -0400

    Midterm

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://Roshan:k4Sbr23Qj2hRoXoA@cluster0.7ikmxda.mongodb.net/books229?retryWrites=true&w=majority"
+};
+
+// k4Sbr23Qj2hRoXoA password
